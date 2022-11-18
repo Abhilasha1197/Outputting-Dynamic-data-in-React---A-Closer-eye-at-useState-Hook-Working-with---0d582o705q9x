f245d9a6e93e144df5d24b2a3b788e06ceecdea4
abhilasha <abhilasha15597@gmail.com>
2022-11-18 10:45:49 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
-//code here
-  return (
+ const[data,setData] = useState(null);
+ const [print,setprint] = useState(true);
+  function getData(val){
+    setData(val.target.value)
+  }
+return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' onChange={getData}></input>
+      <button id='button' onClick={()=>setprint(print)}>Click</button>
+      <p id='text'> Hello my name is {data} and I study at Newton School</p>
     </div>
   )
 }
